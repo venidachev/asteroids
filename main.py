25e26ab85af3d17b8776ac60bc614de203e1a3f6
venidachev <venidachev@gmail.com>
2025-02-06 23:08:55 +0200

    added 60 FPS gameplay

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from constants import *
 
 def main():
     pygame.init()
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -13,6 +14,7 @@ def main():
                 return
         screen.fill("#000000")
         pygame.display.update()
+        dt = clock.tick(60) / 1_000
 
 
 if __name__ == "__main__":
