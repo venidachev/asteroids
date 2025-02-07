b28c9659e3c600041930d7a22739ce986c750f23
venidachev <venidachev@gmail.com>
2025-02-07 11:25:49 +0200

    added player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,12 +9,16 @@ def main():
     dt = 0 # delta time
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("#000000")
-        pygame.display.update()
+        player.draw(screen)
+        pygame.display.flip()
         dt = clock.tick(60) / 1_000
 
 
